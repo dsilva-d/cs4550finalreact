65b8eb529a9acf99184d1dfbca7bf3d60b0282b8
dsilva-d <dsilva.d@northeastern.edu>
2022-04-23 18:43:24 -0400

    first

---
+++ src/components/search/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Outlet} from "react-router-dom";
+
+const Search = () = {
+  return (
+    <a href="tuit.html"
+    className={`btn btn-primary btn-block rounded-pill`}>
+    Search</a>
+  )
+}
+export default Search;
