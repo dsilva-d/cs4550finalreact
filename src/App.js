b68df8c2417b5d7ee1435accd79c1fc897e35726
dsilva-d <dsilva.d@northeastern.edu>
2022-04-23 20:22:41 -0400

    app

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
 import Search from "./components/citi/search";
 import Profile from "./components/citi/profile";
+import Citi from "./components/citi"
+
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 function App() {
   return (
   <BrowserRouter>
-  <div className="container">
-    <Routes>
-        <Route path="/">
-            <Route path="search" element={<Search/>}/>
-            <Route path="profile" element={<Profile/>}/>
-        </Route>
-    </Routes>
-  </div>
+      <div className="container">
+        <Routes>
+            <Route path="/">
+                <Route index element={<Citi/>}/>
+                <Route path="search" element={<Search/>}/>
+                <Route path="profile" element={<Profile/>}/>
+            </Route>
+        </Routes>
+      </div>
   </BrowserRouter>
   );
 }
